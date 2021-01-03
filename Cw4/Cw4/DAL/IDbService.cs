feb2d72a85191f11a908047d812b337eec54befb
s19487 <s19487@pjwstk.edu.pl>
2021-01-03 19:08:47 +0100

    Cwiczenia 4

---
+++ Cw4/Cw4/DAL/IDbService.cs
@@ -0,0 +1,21 @@
+﻿using Cw4.Models;
+using System.Collections.Generic;
+
+
+namespace Cw4.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+        public Student GetStudent(string index);
+
+        public int AddStudent(Student studentToAdd);
+
+        public int RemoveStudent(string index);
+
+        public int UpdateStudent(Student studentToUpdate);
+
+        public StudentsEnrollment StudentsEnrollments(string index);
+
+    }
+}
---
+++ Cw4/Cw4/MODELS/StudentsEnrollment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cw4.Models
+{
+    public class StudentsEnrollment
+    {
+        public int MyProperty { get; set; }
+
+        public string IndexNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public int Semester { get; set; }
+        public DateTime StartDate { get; set; }
+
+    }
+}
